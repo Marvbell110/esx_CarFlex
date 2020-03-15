2660f9375c39cc3f504e7466534d3d30651bf647
Marvbell110 <34713144+Marvbell110@users.noreply.github.com>
2020-03-15 16:48:42 +0100

    Updated to the last esx

--- server/server.lua
+++ server/server.lua
@@ -4,14 +4,14 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent("carflex:payTicket")
 AddEventHandler("carflex:payTicket", function(price, car)
-    local _source = source	
-	local xPlayer = ESX.GetPlayerFromId(_source)
+	local _source = source	
+   	local xPlayer = ESX.GetPlayerFromId(_source)
 	
-    if xPlayer.getMoney() >= price then
-        xPlayer.removeMoney(price)
+    	if xPlayer.getMoney() >= price then
+        	xPlayer.removeMoney(price)
 		TriggerClientEvent("carflex:createcar", _source, car)
-	elseif xPlayer.getBank() >= price then
-		xPlayer.removeBank(price)
+	elseif  xPlayer.getAccount('bank').money >= price then
+		xPlayer.removeAccountMoney('bank', price)
 		TriggerClientEvent("carflex:createcar", _source, car)
 	else
 		TriggerClientEvent("carflex:moneyInvalid", _source)
